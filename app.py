ee3e5caf89dd5c99abac1e25998b5cc30933c9e8
Ling-Cheng-Yan <darkness851106@gmail.com>
2020-06-13 00:29:38 +0800

    four version

--- app.py
+++ app.py
@@ -35,12 +35,21 @@ def callback():
 
     return 'OK'
 
+translator = Translator()
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
+    msg = event.message.text
+    language = translator.detect(msg)
+    if 'lang=ja' in language:
+        line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=translator.translate(msg, dest='en')))
+    elif 'lang=zh-tw' in language:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=translator.translate(msg, dest='en')))
+
 
 
 if __name__ == "__main__":
