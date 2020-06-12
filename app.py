3ba55696277b02c339704ff3774c91eff8713231
Ling-Cheng-Yan <darkness851106@gmail.com>
2020-06-13 02:57:37 +0800

    11 version

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ translator = Translator()
 def handle_message(event):
     msg = event.message.text
     result = translator.translate(msg)
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result.txt)
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result.txt))
 
 if __name__ == "__main__":
     app.run()
